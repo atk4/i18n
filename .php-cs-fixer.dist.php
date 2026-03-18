c3c2817a6e606fe15267b67d075043d8483ff280
Michael Voříšek <mvorisek@mvorisek.cz>
2026-03-18 15:37:46 +0100

    add CI

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,20 +1,21 @@
 <?php
 
-$finder = PhpCsFixer\Finder::create()
-    ->in([__DIR__])
-    ->exclude([
-        'cache',
-        'build',
-        'vendor',
-    ]);
+declare(strict_types=1);
 
-return PhpCsFixer\Config::create()
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
+
+$finder = Finder::create()
+    ->in([__DIR__])
+    ->exclude(['vendor']);
+
+return (new Config())
     ->setRiskyAllowed(true)
     ->setRules([
         '@PhpCsFixer' => true,
-        '@PhpCsFixer:risky' =>true,
-        '@PHP71Migration:risky' => true,
-        '@PHP73Migration' => true,
+        '@PhpCsFixer:risky' => true,
+        '@PHP74Migration' => true,
+        '@PHP74Migration:risky' => true,
 
         // required by PSR-12
         'concat_space' => [
@@ -22,11 +23,6 @@ return PhpCsFixer\Config::create()
         ],
 
         // disable some too strict rules
-        'phpdoc_types' => [
-            // keep enabled, but without "alias" group to not fix
-            // "Callback" to "callback" in phpdoc
-            'groups' => ['simple', 'meta']
-        ],
         'phpdoc_types_order' => [
             'null_adjustment' => 'always_last',
             'sort_algorithm' => 'none',
@@ -36,34 +32,30 @@ return PhpCsFixer\Config::create()
             'equal' => false,
             'identical' => false,
         ],
+        'native_constant_invocation' => true,
         'native_function_invocation' => false,
-        'non_printable_character' => [
-            'use_escape_sequences_in_strings' => true,
-        ],
         'void_return' => false,
         'blank_line_before_statement' => [
             'statements' => ['break', 'continue', 'declare', 'return', 'throw', 'exit'],
         ],
+        'final_internal_class' => false,
         'combine_consecutive_issets' => false,
         'combine_consecutive_unsets' => false,
         'multiline_whitespace_before_semicolons' => false,
         'no_superfluous_elseif' => false,
         'ordered_class_elements' => false,
         'php_unit_internal_class' => false,
-        'php_unit_test_case_static_method_calls' => [
-            'call_type' => 'this',
-        ],
         'php_unit_test_class_requires_covers' => false,
         'phpdoc_add_missing_param_annotation' => false,
         'return_assignment' => false,
         'comment_to_phpdoc' => false,
-        'list_syntax' => ['syntax' => 'short'],
         'general_phpdoc_annotation_remove' => [
             'annotations' => ['author', 'copyright', 'throws'],
         ],
-        'nullable_type_declaration_for_default_null_value' => [
-            'use_nullable_type_declaration' => false,
-        ],
+
+        // fn => without curly brackets is less readable,
+        // also prevent bounding of unwanted variables for GC
+        'use_arrow_functions' => false,
     ])
     ->setFinder($finder)
-    ->setCacheFile(__DIR__ . '/.php_cs.cache');
+    ->setCacheFile(sys_get_temp_dir() . '/php-cs-fixer.' . md5(__DIR__) . '.cache');
